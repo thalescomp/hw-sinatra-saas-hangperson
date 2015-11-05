427daca5740f8dd76b16f02dfe980b04ce13c07f
Thales Moreira Carvalho <thales.comp@gmail.com>
2015-11-05 20:10:02 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,12 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
   def self.get_random_word
@@ -19,4 +22,28 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  def guess(letter)
+    raise ArgumentError if letter.nil? or letter.empty? or not letter.match(/[[:alpha:]]/)
+    letter.downcase!
+    unless @guesses.include? letter or @wrong_guesses.include? letter
+      if @word.include? letter
+        @guesses << letter
+      else
+        @wrong_guesses << letter
+      end
+      return true
+    end
+    
+    false
+  end
+      
+  def word_with_guesses
+    @word.gsub(/[^#{@guesses} ]/,'-')
+  end
+  
+  def check_win_or_lose
+    return :win if @guesses.length == word.length
+    return :lose if @wrong_guesses.length == 7
+    :play
+  end
 end
